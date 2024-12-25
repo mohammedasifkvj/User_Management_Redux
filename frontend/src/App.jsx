d6a8ce14ed84138145ff911e8eb59db22786cd7a
MOHAMMED ASIF K V <mohammedasifkv@gmail.com>
2024-12-25 21:17:46 +0530

    Admin,Redux slice implemented and Admin upto user listing(App.jsx fixed)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,6 @@ import Profile from './pages/Profile';
 
 import AdminSignIn from './pages/AdminSignIn';
 import UsersList from './pages/UsersList';
-import CreateUser from './pages/CreateUser';
-import EditUser from './pages/EditUser';
 
 function App() {
   return (
@@ -25,8 +23,6 @@ function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/list' element={<UsersList />} />
-          <Route path='/createUser' element={<CreateUser />} />
-          <Route path='/editUser/:id' element={<EditUser />} />
         </Route>
       </Routes>
     </BrowserRouter>
